bdfb77a46c200fdca906452b082b8b6624fc529b
janindu_wije <janinduwije21@gmail.com>
2025-07-01 00:20:33 +0530

    added the spalsh screen

--- src/home.js
+++ src/home.js
@@ -5,3 +5,15 @@ window.addEventListener('scroll', () => {
     const moveUp = Math.min(scrollY / 2, 400);
     circle.style.bottom = (-400 + moveUp) + 'px';
 });
+window.addEventListener('load', () => {
+    const splash = document.getElementById('splash-screen');
+    const mainContent = document.getElementById('main-content');
+
+    setTimeout(() => {
+        splash.classList.add('fade-out'); // optional fade
+        setTimeout(() => {
+            splash.style.display = 'none';
+            mainContent.style.display = 'block';
+        }, 1000); // match fade duration
+    }, 2000);
+});
